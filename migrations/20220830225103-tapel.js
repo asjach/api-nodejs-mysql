82cbd39699b0756424e57de780c915fd828a0502
Nurdin Nurdiana Alamsyah <asjach@gmail.com>
2022-08-31 06:07:46 +0700

    membuat skema tabel tapel

--- migrations/20220830225103-tapel.js
+++ migrations/20220830225103-tapel.js
@@ -1,21 +1,29 @@
+//untuk generate file di terminal:
+//npx sequelize migration:create --name namatabel
+
+//untuk mengeksekusi di terminal ketik:
+// npx sequelize db:migrate
+
 'use strict';
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
+
+     await queryInterface.createTable('tapel', { 
+      id: Sequelize.INTEGER, 
+      tapel: {type:Sequelize.STRING(10), allowNull: false},
+      show:{type:Sequelize.BOOLEAN},
+      createdAt:{type: Sequelize.DATE, allowNull: false},
+      updatedAt: {type: Sequelize.DATE, allowNull: false}
+    
+    });
+ 
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+     await queryInterface.dropTable('tapel');
+
   }
 };
