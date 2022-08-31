f32513632b14ae2f1f678a851e98dc81c396ffcb
Nurdin Nurdiana Alamsyah <asjach@gmail.com>
2022-08-31 22:12:46 +0700

    memperbaiki model siswa, membuat model riwayat belajar, membuat relationship di model

--- models/siswa.js
+++ models/siswa.js
@@ -3,20 +3,20 @@ module.exports = (sequelize, DataTypes)=>{
     const Siswa = sequelize.define(
     "Siswa",
     {
-        id: {type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true},
-        nis: {type:DataTypes.STRING, allowNull:false},
-        nama: {type:DataTypes.STRING},
-        jk: {type:DataTypes.STRING},
-        tmp_lahir: {type:DataTypes.STRING},
-        tgl_lahir: {type:DataTypes.DATE},
-        ayah: {type:DataTypes.STRING},
-        ibu: {type:DataTypes.STRING},
-        nisn: {type:DataTypes.STRING(10)},
-        nik: {type:DataTypes.STRING(16)},
-        alamat: {type:DataTypes.STRING}
+        nis: {
+            type:DataTypes.STRING, 
+            allowNull:false,
+            primaryKey: true
+          },
+          nama: {type:DataTypes.STRING},
+          jk: {type:DataTypes.STRING},
+          tmp_lahir: {type:DataTypes.STRING},
+          tgl_lahir: {type:DataTypes.DATE},
+          ayah: {type:DataTypes.STRING},
+          ibu: {type:DataTypes.STRING},
+          nisn: {type:DataTypes.STRING(10)},
+          nik: {type:DataTypes.STRING(16)},
+          alamat: {type:DataTypes.STRING}
     },{tableName: "siswa"}
     );
     return Siswa;
